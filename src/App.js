0d8a4dae5f808e1791f086089bb633ac656c05a4
Arup Pal <aruparchana@Arups-MacBook-Pro.local>
2020-09-04 21:16:03 -0700

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Todo from './components/Todo';
+import Header from './components/layout/Header'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    activities: [
+      {
+        id:1,
+        title:'learning React',
+        completed:false
+      },
+      {
+        id:2,
+        title:'explore React',
+        completed:false
+      },
+      {
+        id:3,
+        title:'implement React',
+        completed:false
+      }]
+  }
+  // toggle item completion
+  toggleComplete = (id) => {
+    this.setState({activities: this.state.activities.map(
+      activity => {
+        if(activity.id === id){
+          activity.completed = !activity.completed
+        }
+        return activity
+      })})}
+
+// delete item.. spread operator is used to copy the entire state and
+// then apply filter only the ones where the id is not equal to the id passed
+// this result is then copied in the state object 
+  deleteItem = (id) =>{
+    this.setState(
+      {activities: [...this.state.activities.filter(activity => activity.id !== id)]} 
+    );
+  }
+  render(){
+    return (
+      <div>
+          <Header/>
+          <Todo todos={this.state.activities} 
+          markComplete={this.toggleComplete}
+          deleteItem = {this.deleteItem}
+          />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
+
+class Todo extends Component{
+    
+
+    render(){
+
+        return this.props.todos.map((acts) =>
+        (            
+           <TodoItem key={acts.id} item={acts} 
+           markComplete = {this.props.markComplete}
+           deleteItem = {this.props.deleteItem}
+           />
+           
+        ));
+    }
+
+}
+
+
+Todo.propTypes = {
+    todos:PropTypes.array.isRequired
+
+}
+
+export default Todo;
